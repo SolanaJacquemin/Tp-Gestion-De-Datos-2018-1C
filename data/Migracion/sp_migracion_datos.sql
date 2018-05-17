91bb095708cd132a3b91a92a07efb5dff2ef9d80
Nicolas Duarte <nicolasduarte93@gmail.com>
2018-05-16 22:10:24 -0300

    Actualizo script de creación de tablas

--- data/Migracion/sp_migracion_datos.sql
+++ data/Migracion/sp_migracion_datos.sql
@@ -1,3 +1,6 @@
+USE GD1C2018
+GO
+
 CREATE PROCEDURE migracion_datos
 AS
 BEGIN
@@ -11,133 +14,364 @@ BEGIN
 	END
 
 	-- Creación de tablas
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Funcionalidades')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Funcionalidad')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Funcionalidades (
-			Funcionalidad_Codigo nvarchar(10) PRIMARY KEY,
-			Funcionalidad_Descripcion nvarchar(255) 
+		CREATE TABLE FOUR_SIZONS.Funcionalidad (
+			Func_Codigo numeric(18,0),
+			Func_Descripcion nvarchar(255),
+			Func_Nombre varchar(50),
+			Func_Estado bit,
+			CONSTRAINT PK_Funcionalidad PRIMARY KEY(Func_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Roles')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Rol')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Roles (
-			Rol_Nombre nvarchar(10) PRIMARY KEY,
-			Rol_Funcionalidad nvarchar(10) REFERENCES FOUR_SIZONS.Funcionalidades(Funcionalidad_Codigo),
-			Rol_Estado char(1) NOT NULL
+		CREATE TABLE FOUR_SIZONS.Rol (
+			Rol_Codigo numeric(18,0),
+			Rol_Nombre nvarchar(50),
+			Rol_Estado bit,
+			CONSTRAINT PK_Rol PRIMARY KEY(Rol_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Usuarios')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'RolXFunc')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.RolXFunc (
+			Rol_Codigo numeric(18,0),
+			Func_Codigo numeric(18,0),
+			RolXFunc_Estado bit,
+
+			CONSTRAINT FK_RolXFunc_1 FOREIGN KEY (Rol_Codigo) REFERENCES FOUR_SIZONS.Rol(Rol_Codigo),
+			CONSTRAINT FK_RolXFunc_2 FOREIGN KEY (Func_Codigo) REFERENCES FOUR_SIZONS.Funcionalidad(Func_Codigo),
+
+			CONSTRAINT PK_RolXFunc PRIMARY KEY(Rol_Codigo, Func_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Usuario')
 		BEGIN
-			CREATE TABLE FOUR_SIZONS.Usuarios (
-				Usuario_Codigo nvarchar(10) PRIMARY KEY,
-				Usuario_Password nvarchar(50) NOT NULL,
-				Usuario_Rol_Asig nvarchar(10) REFERENCES FOUR_SIZONS.Roles(Rol_Nombre) NOT NULL,
-				Usuario_Nombre_Apellido nvarchar(100) NOT NULL,
-				Usuario_Tip_Doc nvarchar(5) NOT NULL,
-				Usuario_Nro_Doc numeric(18) NOT NULL,
-				Usuario_Mail nvarchar(255) NOT NULL,
-				Usuario_Telefono numeric(18) NOT NULL,
+			CREATE TABLE FOUR_SIZONS.Usuario (
+				Usuario_ID nvarchar(15),
+				Usuario_Password nvarchar(15) NOT NULL,
+				Usuario_Nombre nvarchar(50) NOT NULL,
+				Usuario_Apellido nvarchar(50) NOT NULL,
+				Usuario_TipoDoc nvarchar(50) NOT NULL,
+				Usuario_NroDoc numeric(18) NOT NULL,
+				Usuario_Telefono nvarchar(18) NOT NULL,
 				Usuario_Direccion nvarchar(255) NOT NULL,
 				Usuario_Fec_Nac datetime NOT NULL,
-				Usuario_Hotel int NOT NULL
+				Usuario_Mail nvarchar(50) NOT NULL,
+				Usuario_Estado bit NOT NULL,
+				Usuario_FallaLog numeric(1) NOT NULL
+				CONSTRAINT PK_Usuario PRIMARY KEY (Usuario_ID)
 			)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Habitaciones_Tipo')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'UsuarioXRol')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Habitaciones_Tipo (
-			   Habitacion_Tipo_Codigo numeric(18) PRIMARY KEY,
-			   Habitacion_Tipo_Descripcion nvarchar(255),
-			   Habitacion_Tipo_Porcentual numeric(18,2),
+		CREATE TABLE FOUR_SIZONS.UsuarioXRol (
+			Usuario_ID nvarchar(15),
+			Rol_Codigo numeric(18,0),
+			UsuarioXRol_Estado bit,
+
+			CONSTRAINT FK_UsuarioXRol_1 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+			CONSTRAINT FK_UsuarioXRol_2 FOREIGN KEY (Rol_Codigo) REFERENCES FOUR_SIZONS.Rol(Rol_Codigo),
+
+			CONSTRAINT PK_UsuarioXRol PRIMARY KEY (Usuario_ID, Rol_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Regimenes')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Hotel')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Regimenes (
-			Regimen_Codigo int IDENTITY(1,1) PRIMARY KEY,
+		CREATE TABLE FOUR_SIZONS.Hotel (
+			Hotel_Codigo numeric(18) IDENTITY(1,1),
+			Hotel_Nombre nvarchar(50),
+			Hotel_Mail nvarchar(50),
+			Hotel_Telefono nchar(8),
+			Hotel_Calle nvarchar(255),
+			Hotel_Nro_Calle numeric(18),
+			Hotel_CantEstrella numeric(18),
+			Hotel_Recarga_Estrella numeric(18),
+			Hotel_Ciudad nvarchar(255),
+			Hotel_Pais nvarchar(50),			
+			Hotel_FechaCreacion datetime,
+			Hotel_Estado bit
+			CONSTRAINT PK_Hotel PRIMARY KEY (Hotel_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'UsuarioXHotel')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.UsuarioXHotel (
+			Usuario_ID nvarchar(15),
+			Hotel_Codigo numeric(18),
+			UsuarioXHotel_Estado bit,
+
+			CONSTRAINT FK_UsuarioXHotel_1 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+			CONSTRAINT FK_UsuarioXHotel_2 FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+
+			CONSTRAINT PK_UsuarioXHotel PRIMARY KEY (Usuario_ID, Hotel_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Regimen')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Regimen (
+			Regimen_Codigo numeric(18) IDENTITY(1,1),
 			Regimen_Descripcion nvarchar(255),
 			Regimen_Precio numeric(18,2),
 			Regimen_Estado char(1)
+			CONSTRAINT PK_Regimen PRIMARY KEY (Regimen_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Consumibles')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'RegXHotel')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Consumibles (
-			Consumible_Codigo numeric(18) PRIMARY KEY,
+		CREATE TABLE FOUR_SIZONS.RegXHotel (
+			Hotel_Codigo numeric(18),
+			Regimen_Codigo numeric(18,0), 
+			RegXHotel_Estado bit,
+
+			CONSTRAINT FK_RegXHotel_1 FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+			CONSTRAINT FK_RegXHotel_2 FOREIGN KEY (Regimen_Codigo) REFERENCES FOUR_SIZONS.Regimen(Regimen_Codigo),
+
+			CONSTRAINT PK_RegXHotel PRIMARY KEY (Hotel_Codigo, Regimen_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Hotel_Cerrado')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Hotel_Cerrado (
+			Cerrado_codigo numeric(18),
+			Cerrado_FechaI datetime,
+			Cerrado_FechaF datetime,
+			Cerrado_Detalle nvarchar(255),
+			Hotel_Codigo numeric(18),
+
+			CONSTRAINT FK_Hotel_Cerrado FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+
+			CONSTRAINT PK_Hotel_Cerrado PRIMARY KEY (Cerrado_codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Habitacion_Tipo')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Habitacion_Tipo (
+			   Habitacion_Tipo_Codigo numeric(18),
+			   Habitacion_Tipo_Descripcion nvarchar(255),
+			   Habitacion_Tipo_Porcentual numeric(18,2),
+			   CONSTRAINT PK_Habitacion_Tipo PRIMARY KEY (Habitacion_Tipo_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Consumible')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Consumible (
+			Consumible_Codigo numeric(18),
 			Consumible_Descripcion nvarchar(255),
-			Consumible_Precio numeric(18,2)
+			Consumible_Precio numeric(18,2),
+			CONSTRAINT PK_Consumible PRIMARY KEY (Consumible_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Hoteles')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Habitacion')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Hoteles (
-			Hotel_Codigo int IDENTITY(1,1) PRIMARY KEY,
-			Hotel_Nombre nvarchar(255),
-			Hotel_Ciudad nvarchar(255),
-			Hotel_Calle nvarchar(255),
-			Hotel_Nro_Calle numeric(18),
-			Hotel_Telefono numeric(18),
-			Hotel_Email nvarchar(50),
-			Hotel_CantEstrella numeric(18),
-			Hotel_FechaCreacion datetime,
-			Hotel_Recarga_Estrella numeric(18)
-		)
-	END
-
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Habitaciones')
-	BEGIN
-		CREATE TABLE FOUR_SIZONS.Habitaciones (
+		CREATE TABLE FOUR_SIZONS.Habitacion (
 			Habitacion_Numero numeric(18),
+			Hotel_Codigo numeric(18),
 			Habitacion_Piso numeric(18),
-			Habitacion_Hotel int REFERENCES FOUR_SIZONS.Hoteles(Hotel_Codigo),
 			Habitacion_Frente nvarchar(50),
-			Habitacion_Tipo numeric(18)  REFERENCES FOUR_SIZONS.Habitaciones_Tipo(Habitacion_Tipo_Codigo)
-			PRIMARY KEY(Habitacion_Numero, Habitacion_Piso, Habitacion_Hotel)
+			Habitacion_Tipo_Codigo numeric(18),  
+			Habitacion_Estado bit,
+
+			CONSTRAINT FK_Habitacion_1 FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+			CONSTRAINT FK_Habitacion_2 FOREIGN KEY (Habitacion_Tipo_Codigo) REFERENCES FOUR_SIZONS.Habitacion_Tipo(Habitacion_Tipo_Codigo),
+			
+			CONSTRAINT PK_Habitacion PRIMARY KEY (Habitacion_Numero, Hotel_Codigo)
 		)
 	END
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Clientes')
-		CREATE TABLE FOUR_SIZONS.Clientes (
-			Cliente_Nacionalidad nvarchar(50),
-			Cliente_Tipo_Ident nvarchar(5),
-			Cliente_Nro_Ident numeric(18),
-			Cliente_Mail nvarchar(255),
-			Cliente_Apellido nvarchar(255),
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Cliente')
+		CREATE TABLE FOUR_SIZONS.Cliente (
+			Cliente_Codigo numeric(18) IDENTITY,
 			Cliente_Nombre nvarchar(255),
-			Cliente_Fecha_Nac datetime,
+			Cliente_Apellido nvarchar(255),
+			Cliente_TipoDoc nvarchar(255),
+			Cliente_NumDoc numeric(18,0),
 			Cliente_Dom_Calle nvarchar(255),
 			Cliente_Nro_Calle numeric(18),
 			Cliente_Piso numeric(18),
 			Cliente_Depto nvarchar(50),
-			PRIMARY KEY (Cliente_Nacionalidad, Cliente_Tipo_Ident, Cliente_Nro_Ident, Cliente_Mail)
+			Cliente_Mail nvarchar(255),
+			Cliente_Nacionalidad nvarchar(50),
+			Cliente_Fecha_Nac datetime,
+			Cliente_Puntos decimal(18,2),
+			Cliente_Estado nvarchar(5)
+			CONSTRAINT PK_Cliente PRIMARY KEY (Cliente_Codigo)
 		)
 
-	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Reservas')
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Tarjeta')
 	BEGIN
-		CREATE TABLE FOUR_SIZONS.Reservas (
-			Reserva_Codigo numeric(18) NOT NULL,
-			Reserva_Hotel_Codigo int NOT NULL REFERENCES FOUR_SIZONS.Hoteles(Hotel_Codigo),
-			Reserva_Fecha datetime,
+		CREATE TABLE FOUR_SIZONS.Tarjeta (
+			Tarjeta_Numero numeric(18),
+			Tarjeta_Venc datetime,
+			Tarjeta_Cod numeric(3),
+			Tarjeta_Titular nvarchar(50),
+			Tarjeta_Marca nvarchar(50)
+			CONSTRAINT PK_Tarjeta PRIMARY KEY (Tarjeta_Numero)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Reserva')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Reserva (
+			Reserva_Codigo numeric(18),
+			Reserva_FechaCreacion datetime,
+			Reserva_Fecha_Inicio datetime,
+			Reserva_Fecha_Fin datetime,
 			Reserva_Cant_Noches numeric(18),
-			Reserva_Fecha_Desde datetime,
-			Reserva_Fecha_Hasta datetime,
-			Reserva_Tipo_Habitacion numeric(18) REFERENCES FOUR_SIZONS.Habitaciones_Tipo(Habitacion_Tipo_Codigo),
-			Reserva_Tipo_Regimen int REFERENCES FOUR_SIZONS.Regimenes(Regimen_Codigo),
-			Reserva_Cli_Nac nvarchar(50),
-			Reserva_Cli_Tipo_Ident nvarchar(5),
-			Reserva_Cli_Nro_Ident numeric(18)
-			PRIMARY KEY(Reserva_Codigo, Reserva_Hotel_Codigo)
+			Reserva_Precio decimal(12,2),
+			Usuario_ID nvarchar(15),
+			Hotel_Codigo numeric(18),
+			Cliente_Codigo numeric(18),
+			Reserva_Estado bit,
+
+			CONSTRAINT FK_Reserva_1 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+			CONSTRAINT FK_Reserva_2 FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+			CONSTRAINT FK_Reserva_3 FOREIGN KEY (Cliente_Codigo) REFERENCES FOUR_SIZONS.Cliente(Cliente_Codigo),
+
+			CONSTRAINT PK_Reserva PRIMARY KEY (Reserva_Codigo)
 		)		
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'ReservaMod')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.ReservaMod (
+			Reserva_Codigo numeric(18),
+			Usuario_ID nvarchar(15),
+			ResMod_Detalle nvarchar(255),
+			ResMod_Fecha datetime,
+			
+			CONSTRAINT FK_ReservaMod_1 FOREIGN KEY (Reserva_Codigo) REFERENCES FOUR_SIZONS.Reserva(Reserva_Codigo),
+			CONSTRAINT FK_ReservaMod_2 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+
+			CONSTRAINT PK_ReservaMod PRIMARY KEY (Reserva_Codigo, Usuario_ID)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Habitacion_TipoXReser')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Habitacion_TipoXReser (
+			Reserva_Codigo numeric(18),
+			Habitacion_Tipo_Codigo numeric(18),			
+
+			CONSTRAINT FK_Habitacion_TipoXReser_1 FOREIGN KEY (Reserva_Codigo) REFERENCES FOUR_SIZONS.Reserva(Reserva_Codigo),
+			CONSTRAINT FK_Habitacion_TipoXReser_2 FOREIGN KEY (Habitacion_Tipo_Codigo) REFERENCES FOUR_SIZONS.Habitacion_Tipo(Habitacion_Tipo_Codigo),
+
+			CONSTRAINT PK_Habitacion_TipoXReser PRIMARY KEY (Reserva_Codigo, Habitacion_Tipo_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Estadia')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Estadia (
+			Estadia_Codigo numeric(18),
+			Reserva_Codigo numeric(18), 
+			Estadia_FechaInicio datetime,
+			Estadia_FechaFin datetime,
+			Estadia_CantNoches numeric(18),
+			Usuario_ID nvarchar(15),
+			Habitacion_Numero numeric(18),
+			Hotel_Codigo numeric(18),
+
+			CONSTRAINT FK_Estadia_1 FOREIGN KEY (Reserva_Codigo) REFERENCES FOUR_SIZONS.Reserva(Reserva_Codigo),
+			CONSTRAINT FK_Estadia_2 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+			CONSTRAINT FK_Estadia_3 FOREIGN KEY (Habitacion_Numero, Hotel_Codigo) REFERENCES FOUR_SIZONS.Habitacion(Habitacion_Numero, Hotel_Codigo),
+
+			CONSTRAINT PK_Estadia PRIMARY KEY (Estadia_Codigo)
+
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'EstadiaXCliente')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.EstadiaXCliente (
+			Cliente_Codigo numeric(18),
+			Estadia_Codigo numeric(18),
+
+			CONSTRAINT FK_EstadiaXCliente_1 FOREIGN KEY (Cliente_Codigo) REFERENCES FOUR_SIZONS.Cliente(Cliente_Codigo),
+			CONSTRAINT FK_EstadiaXCliente_2 FOREIGN KEY (Estadia_Codigo) REFERENCES FOUR_SIZONS.Estadia(Estadia_Codigo),
+
+			CONSTRAINT PK_EstadiaXCliente PRIMARY KEY (Cliente_Codigo, Estadia_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'EstadiaXConsumible')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.EstadiaXConsumible (
+			Estadia_Codigo numeric(18),
+			Consumible_Codigo numeric(18),
+
+			CONSTRAINT FK_EstadiaXConsumible_1 FOREIGN KEY (Estadia_Codigo) REFERENCES FOUR_SIZONS.Estadia(Estadia_Codigo),
+			CONSTRAINT FK_EstadiaXConsumible_2 FOREIGN KEY (Consumible_Codigo) REFERENCES FOUR_SIZONS.Consumible(Consumible_Codigo),
+
+			CONSTRAINT PK_EstadiaXConsumible PRIMARY KEY (Estadia_Codigo, Consumible_Codigo)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Factura')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Factura (
+			Factura_Nro numeric(18),
+			Factura_Fecha datetime,
+			Factura_Total decimal(18,2),
+			Factura_FormaPago nvarchar(50),
+			Usuario_ID nvarchar(15),
+			Estadia_Codigo numeric(18),
+			Cliente_Codigo numeric(18),
+
+			CONSTRAINT FK_Factura_1 FOREIGN KEY (Usuario_ID) REFERENCES FOUR_SIZONS.Usuario(Usuario_ID),
+			CONSTRAINT FK_Factura_2 FOREIGN KEY (Estadia_Codigo) REFERENCES FOUR_SIZONS.Estadia(Estadia_Codigo),
+			CONSTRAINT FK_Factura_3 FOREIGN KEY (Cliente_Codigo) REFERENCES FOUR_SIZONS.Cliente(Cliente_Codigo),
+
+			CONSTRAINT PK_Factura PRIMARY KEY (Factura_Nro)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Item_Factura')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Item_Factura (
+			Factura_Nro numeric(18),
+			Item_Factura_NroItem numeric(18),
+			Item_Factura_Cant numeric(18),
+			Item_Factura_Monto decimal(18,2),
+
+
+			CONSTRAINT FK_Item_Factura FOREIGN KEY (Factura_Nro) REFERENCES FOUR_SIZONS.Factura(Factura_Nro),
+			
+			CONSTRAINT PK_Item_Factura PRIMARY KEY (Factura_Nro, Item_Factura_NroItem)
+		)
+	END
+
+	IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Disponibilidad')
+	BEGIN
+		CREATE TABLE FOUR_SIZONS.Disponibilidad (
+			Disp_Fecha numeric(18),
+			Habitacion_Tipo_Codigo numeric(18),
+			Hotel_Codigo numeric(18),
+			Disp_HabDisponibles numeric(3),
+			
+			CONSTRAINT FK_Disponibilidad_1 FOREIGN KEY (Habitacion_Tipo_Codigo) REFERENCES FOUR_SIZONS.Habitacion_Tipo(Habitacion_Tipo_Codigo),
+			CONSTRAINT FK_Disponibilidad_2 FOREIGN KEY (Hotel_Codigo) REFERENCES FOUR_SIZONS.Hotel(Hotel_Codigo),
+
+			CONSTRAINT PK_Disponibilidad PRIMARY KEY (Disp_Fecha, Habitacion_Tipo_Codigo, Hotel_Codigo)
+		)
 	END
 
 	-- Migración de datos de la tabla maestra
 	-- Tipos de Habitaciones
-	INSERT INTO FOUR_SIZONS.Habitaciones_Tipo (Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual) 
+	/**INSERT INTO FOUR_SIZONS.Habitaciones_Tipo (Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual) 
 	SELECT DISTINCT Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion, Habitacion_Tipo_Porcentual
 	FROM GD1C2018.gd_esquema.Maestra
 	ORDER BY Habitacion_Tipo_Codigo
@@ -184,8 +418,6 @@ BEGIN
 	FROM GD1C2018.gd_esquema.Maestra AS M
 	JOIN FOUR_SIZONS.Hoteles AS H ON H.Hotel_Ciudad = M.Hotel_Ciudad and H.Hotel_Nro_Calle = M.Hotel_Nro_Calle
 	JOIN FOUR_SIZONS.Regimenes AS R ON R.Regimen_Descripcion = M.Regimen_Descripcion
-	ORDER BY H.Hotel_Codigo
+	ORDER BY H.Hotel_Codigo**/
 
 END
-
-EXEC migracion_datos
