2b0017f9db0ec5da30fafb3107928d264389c192
Nicolas Duarte <nicolasduarte93@gmail.com>
2018-05-27 20:28:49 -0300

    Conexión de la app con la base de datos

---
+++ src/FrbaHotel/Conexion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FrbaHotel
+{
+    class Conexion
+    {
+        public string cadenaconexion;
+        public string query = "";
+        public SqlCommand command;
+        public SqlConnection cnx;
+        public SqlDataReader lector;
+
+        public Conexion()
+        {
+            //"Data Source=localhost\\SQLSERVER20012;Initial Catalog=GD1C2018;User ID=gdHotel2018;Password=gd2018";
+            this.cadenaconexion = lectorConfig.Config.cadenaConexion();
+            this.cnx = new SqlConnection(this.cadenaconexion);
+        }
+
+        public void comandear()
+        {
+            command = new SqlCommand(query, cnx);
+        }
+
+        public void abrirConexion()
+        {
+            cnx.Open();
+        }
+        public void cerrarConexion()
+        {
+            cnx.Close();
+        }
+        /*Ejecuta un query que no devuleve datos(UPDATE, INSERT, DELETE, etc)*/
+        public void ejecutarNoQuery()
+        {
+            this.abrirConexion();
+            comandear();
+            command.ExecuteNonQuery();
+            this.cerrarConexion();
+        }
+
+        /*Ejecuta un query que devuelva datos(SELECT)*/
+        /*Despues de ejecutar este metodo y terminar de usar el Reader*/
+        /*SIEMPRE utilizar cerrarConexion */
+        public void ejecutarQuery()
+        {
+            this.abrirConexion();
+            comandear();
+            lector = command.ExecuteReader();
+        }
+
+        public bool leerReader()
+        {
+            return lector.Read();
+        }
+    }
+}
--- src/FrbaHotel/Form1.cs
+++ src/FrbaHotel/Form1.cs
@@ -12,9 +12,28 @@ namespace FrbaHotel
 {
     public partial class Form1 : Form
     {
+        private Conexion con = new Conexion();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //InitializeComponent();
+            //Conexion con = new Conexion();
+            con.query = "SELECT Cliente_Nombre, Cliente_Apellido, Cliente_NumDoc FROM FOUR_SIZONS.Cliente WHERE Cliente_Codigo = 1";
+            con.ejecutarQuery();
+            con.leerReader();
+            textBox1.Text = con.lector.GetString(0);
+            textBox2.Text = con.lector.GetString(1);
+            textBox3.Text = con.lector.GetDecimal(2).ToString();
+            con.cerrarConexion();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
