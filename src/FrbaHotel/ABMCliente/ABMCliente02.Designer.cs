1c3e8a1d754b3bbe863e89a10ffcc3215767be6e
Janice <janicebahiana@gmail.com>
2018-06-22 13:13:11 -0300

    Se agrega ABM cliente

---
+++ src/FrbaHotel/ABMCliente/ABMCliente02.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace FrbaHotel.ABMCliente
+{
+    partial class ABMCliente02
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cb_tipo_doc = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_mail = new System.Windows.Forms.TextBox();
+            this.txt_nacionalidad = new System.Windows.Forms.TextBox();
+            this.txt_telefono = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_nro_doc = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dt_fecha_nac = new System.Windows.Forms.DateTimePicker();
+            this.btn_volver_Click = new System.Windows.Forms.Button();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_aceptar_nuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 8F);
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "FRBA Hoteles";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 12F);
+            this.label11.Location = new System.Drawing.Point(12, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 19);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "FOUR SIZONS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Teléfono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Mail";
+            // 
+            // cb_tipo_doc
+            // 
+            this.cb_tipo_doc.FormattingEnabled = true;
+            this.cb_tipo_doc.Location = new System.Drawing.Point(124, 89);
+            this.cb_tipo_doc.Name = "cb_tipo_doc";
+            this.cb_tipo_doc.Size = new System.Drawing.Size(121, 21);
+            this.cb_tipo_doc.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(260, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Nro. Doc.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Dirección";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(124, 63);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(121, 20);
+            this.txt_nombre.TabIndex = 26;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 92);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Tipo Doc.";
+            // 
+            // txt_mail
+            // 
+            this.txt_mail.Location = new System.Drawing.Point(124, 116);
+            this.txt_mail.Name = "txt_mail";
+            this.txt_mail.Size = new System.Drawing.Size(121, 20);
+            this.txt_mail.TabIndex = 28;
+            // 
+            // txt_nacionalidad
+            // 
+            this.txt_nacionalidad.Location = new System.Drawing.Point(124, 195);
+            this.txt_nacionalidad.Name = "txt_nacionalidad";
+            this.txt_nacionalidad.Size = new System.Drawing.Size(121, 20);
+            this.txt_nacionalidad.TabIndex = 29;
+            // 
+            // txt_telefono
+            // 
+            this.txt_telefono.Location = new System.Drawing.Point(124, 144);
+            this.txt_telefono.Name = "txt_telefono";
+            this.txt_telefono.Size = new System.Drawing.Size(121, 20);
+            this.txt_telefono.TabIndex = 30;
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(124, 170);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(121, 20);
+            this.txt_direccion.TabIndex = 32;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(320, 63);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(121, 20);
+            this.txt_apellido.TabIndex = 33;
+            // 
+            // txt_nro_doc
+            // 
+            this.txt_nro_doc.Location = new System.Drawing.Point(320, 90);
+            this.txt_nro_doc.Name = "txt_nro_doc";
+            this.txt_nro_doc.Size = new System.Drawing.Size(121, 20);
+            this.txt_nro_doc.TabIndex = 34;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(55, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 13);
+            this.label10.TabIndex = 36;
+            this.label10.Text = "Fecha Nac.";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(49, 198);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(69, 13);
+            this.label12.TabIndex = 37;
+            this.label12.Text = "Nacionalidad";
+            // 
+            // dt_fecha_nac
+            // 
+            this.dt_fecha_nac.Location = new System.Drawing.Point(124, 221);
+            this.dt_fecha_nac.Name = "dt_fecha_nac";
+            this.dt_fecha_nac.Size = new System.Drawing.Size(213, 20);
+            this.dt_fecha_nac.TabIndex = 38;
+            this.dt_fecha_nac.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btn_volver_Click
+            // 
+            this.btn_volver_Click.Location = new System.Drawing.Point(366, 292);
+            this.btn_volver_Click.Name = "btn_volver_Click";
+            this.btn_volver_Click.Size = new System.Drawing.Size(75, 23);
+            this.btn_volver_Click.TabIndex = 39;
+            this.btn_volver_Click.Text = "Volver";
+            this.btn_volver_Click.UseVisualStyleBackColor = true;
+            this.btn_volver_Click.Click += new System.EventHandler(this.btn_volver_Click_Click);
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Calibri", 20F);
+            this.lbl_titulo.Location = new System.Drawing.Point(190, 12);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(77, 33);
+            this.lbl_titulo.TabIndex = 40;
+            this.lbl_titulo.Text = "Titulo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_aceptar_nuevo
+            // 
+            this.btn_aceptar_nuevo.Location = new System.Drawing.Point(133, 292);
+            this.btn_aceptar_nuevo.Name = "btn_aceptar_nuevo";
+            this.btn_aceptar_nuevo.Size = new System.Drawing.Size(112, 23);
+            this.btn_aceptar_nuevo.TabIndex = 42;
+            this.btn_aceptar_nuevo.Text = "Aceptar y nuevo";
+            this.btn_aceptar_nuevo.UseVisualStyleBackColor = true;
+            // 
+            // ABMCliente02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 327);
+            this.Controls.Add(this.btn_aceptar_nuevo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_titulo);
+            this.Controls.Add(this.btn_volver_Click);
+            this.Controls.Add(this.dt_fecha_nac);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt_nro_doc);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_direccion);
+            this.Controls.Add(this.txt_telefono);
+            this.Controls.Add(this.txt_nacionalidad);
+            this.Controls.Add(this.txt_mail);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cb_tipo_doc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label11);
+            this.Name = "ABMCliente02";
+            this.Text = "Alta Cliente";
+            this.Load += new System.EventHandler(this.ABMCliente02_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cb_tipo_doc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_mail;
+        private System.Windows.Forms.TextBox txt_nacionalidad;
+        private System.Windows.Forms.TextBox txt_telefono;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_nro_doc;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker dt_fecha_nac;
+        private System.Windows.Forms.Button btn_volver_Click;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_aceptar_nuevo;
+    }
+}
---
+++ src/FrbaHotel/ABMCliente/ABMCliente02.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FrbaHotel.ABMCliente;
+using FrbaHotel.Prompts;
+
+namespace FrbaHotel.ABMCliente
+{
+
+    public partial class ABMCliente02 : Form
+    {
+        public static string modoABM;
+        public string nombre_sp;
+        public int error;
+        public string cliente;
+
+        public ABMCliente02(string modo)
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            modoABM = modo;
+
+            switch (modoABM)
+            {
+                case "INS":
+                    lbl_titulo.Text = "Alta de Cliente";
+                    break;
+
+                case "DLT":
+                    lbl_titulo.Text = "Baja de Cliente";
+                    txt_nombre.ReadOnly = true;
+                    txt_apellido.ReadOnly = true;
+                    txt_mail.ReadOnly = true;
+                    txt_nro_doc.ReadOnly = true;
+                    cb_tipo_doc.Enabled = false;
+                    txt_telefono.ReadOnly = true;
+                    txt_direccion.ReadOnly = true;
+                    dt_fecha_nac.Enabled = false;
+                    txt_nacionalidad.ReadOnly = true;
+                    btn_aceptar_nuevo.Visible = false;
+                    break;
+
+                case "UPD":
+                    lbl_titulo.Text = "Modificación de Cliente";
+                    btn_aceptar_nuevo.Visible = false;
+                    break;
+            }
+
+            dt_fecha_nac.Format = DateTimePickerFormat.Custom;
+            dt_fecha_nac.CustomFormat = "dd/MM/yyyy";
+
+
+        }
+
+        private void ABMCliente02_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_volver_Click_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
