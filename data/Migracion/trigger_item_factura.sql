3a1fc22a46870bc937021cc48e13001b68936cf3
Nicolas Duarte <nicolasduarte93@gmail.com>
2018-07-07 19:33:31 -0300

    trigger factura (intento)

--- data/Migracion/trigger_item_factura.sql
+++ data/Migracion/trigger_item_factura.sql
@@ -1,30 +1,52 @@
-create trigger t2 on FOUR_SIZONS.Item_Factura
-instead of insert
+alter trigger FOUR_SIZONS.corrigeItemFactura on FOUR_SIZONS.Item_Factura instead of insert
 as
 begin
-	declare @nroitem as numeric(18)
-	declare @nroitemn as numeric(18)
-	
-	declare @facnro as numeric(18)
-	declare @cant as numeric(18)
-	declare @monto as decimal(18,2)
+	create table #item(
+		Nro numeric(18),
+		NroItem numeric(18),
+		Cant numeric(18),
+		Descripcion nvarchar(50),
+		Monto decimal(18,2)
+	)
 
-	declare @facnroant as numeric(18)
-	set @nroitemn = 1
+	insert into #item
+	select * from inserted i order by i.Factura_Nro, i.Item_Factura_NroItem
 
-	select top 1 @facnro = Factura_Nro, @nroitem = Item_Factura_NroItem, @cant = Item_Factura_Cant, @monto = Item_Factura_Monto 
-	from inserted
-	order by Item_Factura_NroItem desc
+	declare c1 cursor for 
+	select it.Nro, it.NroItem, it.Cant, it.Descripcion, it.Monto from #item it
 
-	if  exists (select * from FOUR_SIZONS.Item_Factura where Factura_Nro = @facnro)
+	declare @factura numeric(18)
+	declare @nro_item numeric(18)
+	declare @cant numeric(18)
+	declare @descr nvarchar(50)
+	declare @monto decimal(18,2)
+
+	declare @factura_ant numeric(18)
+	declare @item_factura decimal(18)
+	set @item_factura = 0
+
+	open c1
+	fetch next from c1 into @factura, @nro_item, @cant, @descr, @monto
+
+	set @factura_ant = @factura
+
+	while @@FETCH_STATUS = 0
 	begin
-		select @nroitem = Item_Factura_NroItem from FOUR_SIZONS.Item_Factura where Factura_Nro = @facnro
-		set @nroitem = @nroitem + 1
-		insert into FOUR_SIZONS.Item_Factura values (@facnro, @nroitem, @cant, @monto)
+		if(@factura = @factura_ant)
+		begin
+			set @item_factura = @item_factura + 1
+			insert into FOUR_SIZONS.Item_Factura values (@factura, @item_factura, @cant, @descr, @monto)
+		end
+		else
+		begin
+			set @item_factura = 1
+			insert into FOUR_SIZONS.Item_Factura values (@factura, @item_factura, @cant, @descr, @monto)
+			set @factura_ant = @factura
+		end
+		fetch next from c1 into @factura, @nro_item, @cant, @descr, @monto
 	end
-	else
-	begin
-		insert into FOUR_SIZONS.Item_Factura values (@facnro, @nroitemn, @cant, @monto)
-	end
+
+	close c1
+	deallocate c1
 
 end
